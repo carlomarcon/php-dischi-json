4d83ee20cb778a3e092c0a70ccf6b63b74664530
carlomarcon <carlomarcon1998@gmail.com>
2023-12-14 23:34:04 +0100

    ADD: SERVER.PHP

--- server.php
+++ server.php
@@ -1,3 +1,12 @@
 <?php
-$json=file_get_contents('todos.json');
-var_dump($json);
+$jsson=file_get_contents('todos.json');
+// var_dump($jsson);
+$jsonArray=json_decode($jsson,true);
+// var_dump($jsonArray);
+
+$json=json_encode($jsonArray);
+
+// var_dump($json);
+
+header('Content-Type: application/json');
+echo $json;
